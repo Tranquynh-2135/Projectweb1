a333b24a0f3245d9f78c0c93916f852fe92c3b67
quqynh <quynhtran2135@gmail.com>
2024-11-08 13:08:22 +0700

    Initial commit

---
+++ src/main/java/com/example/WebAnime/WebAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebAnime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebAnime/WebAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebAnime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
